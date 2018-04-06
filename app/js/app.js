8f40668ab966585eaaf1f319578d20dd3bbd744c
Gre <gregre@Gres-MacBook-Air.local>
2018-04-07 04:09:07 +1200

    Added cells

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,12 @@
-console.log("FE")
+
+
+
+//If there is any text inside field, crete new task
+function addItem() {
+
+	var text = document.getElementById("item").value;
+	console.log(text);
+}
+
+document.getElementById('addItem').addEventListener('click', addItem);
+
